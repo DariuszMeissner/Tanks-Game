be9a7a5686d8c95fe7dc06e6e8e291c79a39fb37
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-05-06 09:39:56 +0200

    added collision with wall and bot

--- BulletController.js
+++ BulletController.js
@@ -3,23 +3,62 @@ import Bullet from "./bullet.js";
 export default class BulletController {
   bullets = [];
   timerToNextBullet = 0;
-  constructor(ctx) {
+  constructor(ctx, mapController, enemyController) {
     this.ctx = ctx;
+    this.mapController = mapController;
+    this.enemyController = enemyController;
+  }
+
+  draw(ctx) {
+    this.bullets.forEach(bullet => {
+      if (this.isBulletOutOfScreen(bullet)) {
+        this.removeBulletOutOfScreen(bullet)
+      }
+
+      bullet.draw(ctx);
+      this.detectCollisionBot();
+      this.detectCollisionWithWall();
+    })
   }
 
   shoot(x, y, speed, damage, delay, direction) {
-    if (this.timerToNextBullet <= 0) {
+    if (this.bullets.length === 0) {
       this.bullets.push(new Bullet(x, y, damage, speed, direction))
-      this.timerToNextBullet = delay;
     }
-    this.timerToNextBullet--;
   }
+
+  detectCollisionWithWall() {
+    if (this.mapController.collisionWallWithBullet) {
+      this.bullets = [];
+      this.mapController.collisionWallWithBullet = false;
+    }
+  }
+
+  detectCollisionBot() {
+    this.bullets.forEach(bullet => {
+      this.enemyController.enemies.forEach((bot, index) => {
+        const isColision =
+          (bullet.x >= bot.x)
+          && (bullet.y >= bot.y)
+          && (bullet.x + bullet.width <= bot.x + bot.width)
+          && (bullet.y + bullet.height <= bot.y + bot.height)
+
+
+        if (isColision) {
+          this.enemyController.enemies.splice(index, 1);
+          this.bullets = [];
+        }
+
+      })
+    });
+  }
+
 
   isBulletOutOfScreen(bullet) {
     return bullet.y <= -bullet.height
-      || bullet.y >= 320 + bullet.height
-      || bullet.x <= bullet.height
-      || bullet.x >= 640 + bullet.width;
+      || bullet.y >= this.mapController.canvasHeight + bullet.height
+      || bullet.x <= -bullet.width
+      || bullet.x >= this.mapController.canvasWidth + bullet.width;
   }
 
   removeBulletOutOfScreen(bullet) {
@@ -27,13 +66,4 @@ export default class BulletController {
     this.bullets.splice(index, 1);
   }
 
-  draw(ctx) {
-    this.bullets.forEach(bullet => {
-      if (this.isBulletOutOfScreen(bullet)) {
-        this.removeBulletOutOfScreen()
-      }
-
-      bullet.draw(ctx)
-    })
-  }
 }
