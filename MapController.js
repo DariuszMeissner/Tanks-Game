0cec6f54cb11f5fe1c6eca8016ba36ad8cd24c0b
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-04 14:34:06 +0200

    call function detectCollisionPlayerWithBot

--- MapController.js
+++ MapController.js
@@ -21,16 +21,14 @@ export default class MapController {
   }
 
   #drawMap(ctx, player, bullet, enemies) {
-
     for (let row = 0; row < this.map.length; row++) {
       for (let column = 0; column < this.map[row].length; column++) {
-        const tile = this.map[row][column]
-
+        const tile = this.map[row][column];
 
         switch (tile) {
           case mapObject.wall:
-            const wall = new MapElement(column * this.tileSize, row * this.tileSize, this.tileSize, 'brown')
-            wall.draw(ctx)
+            const wall = new MapElement(column * this.tileSize, row * this.tileSize, this.tileSize, "brown");
+            wall.draw(ctx);
             wall.detectCollisionWithPlayer(wall, player);
             wall.detectCollisionWithBot(wall, enemies);
 
@@ -42,18 +40,22 @@ export default class MapController {
             }
             break;
           case mapObject.water:
-            const water = new MapElement(column * this.tileSize, row * this.tileSize, this.tileSize, 'blue');
+            const water = new MapElement(column * this.tileSize, row * this.tileSize, this.tileSize, "blue");
             water.draw(ctx);
             water.detectCollisionWithPlayer(water, player);
             water.detectCollisionWithBot(water, enemies);
             break;
           case mapObject.grass:
-            const grass = new MapElement(column * this.tileSize, row * this.tileSize, this.tileSize, '#58d30da1');
+            const grass = new MapElement(column * this.tileSize, row * this.tileSize, this.tileSize, "#58d30da1");
             grass.draw(ctx);
             break;
           case mapObject.eagle:
-            const eagle = new MapElement(column * this.tileSize, row * this.tileSize, this.tileSize, 'gray');
+            const eagle = new MapElement(column * this.tileSize, row * this.tileSize, this.tileSize, "gray");
             eagle.draw(ctx);
+            break;
+          case mapObject.road:
+            const road = new MapElement(column * this.tileSize, row * this.tileSize, this.tileSize, "black");
+            road.detectCollisionPlayerWithBot(player, enemies);
             break;
         }
       }
