43e6365e5b4decfeedce295229048c788811af36
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-04 14:35:12 +0200

    added map controller for Bot object

--- main.js
+++ main.js
@@ -4,17 +4,11 @@ import Bot from "./Bot.js";
 import BotController from "./BotController.js";
 import MapController from "./MapController.js";
 
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 const tileSize = 40;
 
-export const mapObject = { road: 0, wall: 1, water: 2, grass: 3, eagle: 4 }
-
-const enemies = [
-  new Bot(120, 120, tileSize, tileSize, 'yellow'),
-  new Bot(120, 180, tileSize, tileSize, 'pink'),
-  // new Bot(380, 120, tileSize, tileSize, 'pink'),
-]
+export const mapObject = { road: 0, wall: 1, water: 2, grass: 3, eagle: 4 };
 
 // 0 - road
 // 1 - wall
@@ -35,23 +29,28 @@ const map = [
   [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
   [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1],
-]
+];
 
 const mapController = new MapController(map, tileSize);
-const botController = new BotController(enemies)
+const enemies = [
+  new Bot(120, 120, tileSize, tileSize, "yellow", mapController),
+  new Bot(120, 180, tileSize, tileSize, "pink", mapController),
+  new Bot(380, 120, tileSize, tileSize, "violet", mapController),
+  new Bot(320, 80, tileSize, tileSize, "orange", mapController),
+];
+const botController = new BotController(enemies);
 const bulletController = new BulletController(ctx, mapController, botController);
-const tank = new Tank(160, 80, tileSize, tileSize, bulletController, mapController)
-
+const tank = new Tank(160, 80, tileSize, tileSize, bulletController, mapController);
 
 function gameLoop() {
   mapController.draw(canvas, ctx, tank, bulletController.bullets[0], botController.enemies);
   tank.draw(ctx);
-  bulletController.draw(ctx)
+  bulletController.draw(ctx);
   botController.draw(ctx);
 
   requestAnimationFrame(gameLoop);
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  gameLoop()
-})
+document.addEventListener("DOMContentLoaded", () => {
+  gameLoop();
+});
