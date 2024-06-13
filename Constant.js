b0aa41e277bc682a58b75ce5ae55ac4ca5f9150d
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-13 09:52:27 +0200

    created seperate file for contant

---
+++ Constant.js
@@ -0,0 +1,11 @@
+const MOVEMENT = {
+  forward: "forward",
+  reverse: "reverse",
+  left: "left",
+  right: "right",
+  space: "Space",
+};
+
+const MAP_OBJECT = { road: 0, wall: 1, water: 2, grass: 3, eagle: 4 };
+
+export { MOVEMENT, MAP_OBJECT };
