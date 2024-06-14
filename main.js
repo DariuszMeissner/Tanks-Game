56501ab38a079575750bb2d2a143d1ccc1df40a6
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-14 12:32:16 +0200

    pass tank object to bulletController for unblocking direction

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import Tank from "./Tank.js";
-import BulletController from "./BulletController.js";
 import Bot from "./Bot.js";
+import BulletController from "./BulletController.js";
 import BotController from "./BotController.js";
 import MapController from "./MapController.js";
 
@@ -8,44 +8,47 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 const tileSize = 40;
 
-export const mapObject = { road: 0, wall: 1, water: 2, grass: 3, eagle: 4 };
-
 // 0 - road
 // 1 - wall
 // 2 - water
 // 3 - grass
 // 4 - eagle
+// 5 - edge of map
+// 6 - rock
+// 7 - eagle dead
 
 const map = [
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
-  [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 1],
-  [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 1],
-  [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 1],
-  [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 1],
-  [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 1],
-  [1, 0, 1, 1, 1, 1, 3, 1, 1, 1, 1, 2, 2, 0, 1],
-  [1, 0, 1, 0, 1, 1, 3, 1, 0, 1, 0, 1, 1, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1],
-  [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
-  [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1],
+  [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+  [5, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 5],
+  [5, 0, 1, 0, 0, 0, 1, 0, 0, 0, 2, 2, 2, 0, 5],
+  [5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5],
+  [5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 5],
+  [5, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 5],
+  [5, 0, 1, 1, 6, 1, 6, 1, 1, 1, 0, 0, 0, 0, 5],
+  [5, 0, 1, 0, 6, 1, 6, 1, 0, 1, 0, 1, 1, 0, 5],
+  [5, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 5],
+  [5, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 5],
+  [5, 0, 1, 1, 0, 0, 1, 4, 1, 0, 0, 0, 0, 0, 5],
+  [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
 ];
 
 const mapController = new MapController(map, tileSize);
 const enemies = [
-  new Bot(120, 120, tileSize, tileSize, "yellow", mapController),
-  new Bot(120, 180, tileSize, tileSize, "pink", mapController),
-  new Bot(380, 120, tileSize, tileSize, "violet", mapController),
-  new Bot(320, 80, tileSize, tileSize, "orange", mapController),
+  new Bot(130, 50, tileSize, tileSize, mapController),
+  new Bot(130, 180, tileSize, tileSize, mapController),
+  new Bot(320, 120, tileSize, tileSize, mapController),
+  new Bot(320, 180, tileSize, tileSize, mapController),
+  new Bot(270, 40, tileSize, tileSize, mapController),
+  new Bot(230, 40, tileSize, tileSize, mapController),
 ];
 const botController = new BotController(enemies);
 const bulletController = new BulletController(ctx, mapController, botController);
-const tank = new Tank(160, 80, tileSize, tileSize, bulletController, mapController);
+const tank = new Tank(160, 120, tileSize, tileSize, bulletController, mapController);
 
 function gameLoop() {
   mapController.draw(canvas, ctx, tank, bulletController.bullets[0], botController.enemies);
   tank.draw(ctx);
-  bulletController.draw(ctx);
+  bulletController.draw(ctx, tank);
   botController.draw(ctx);
 
   requestAnimationFrame(gameLoop);
