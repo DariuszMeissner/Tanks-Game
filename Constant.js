a32535242af0bcbb078a0a20507d29e051c57eff
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-18 14:06:50 +0200

    added designs of map elemnts

--- Constant.js
+++ Constant.js
@@ -6,11 +6,21 @@ const MOVEMENT = {
   space: "Space",
 };
 
-const MAP_OBJECT = { road: 0, wall: 1, water: 2, grass: 3, eagle: 4 };
-
+const MAP_OBJECT = { road: 0, wall: 1, water: 2, grass: 3, eagle: 4, mapEdge: 5, rock: 6, eagleDead: 7 };
+const ASSETS = {
+  road: "",
+  wall: "./assets/brick_square.png",
+  water: "./assets/water_square.png",
+  grass: "./assets/grass_square.png",
+  eagle: "./assets/eagle_live.png",
+  eagleDead: "./assets/eagle_dead.png",
+  mapEdge: "",
+  rock: "./assets/rock_square.png",
+};
 const RESET_SPEED = 2;
 const DELAY_START = 2000;
+const COMPENSE_MAP_EDGE_SIZE = 1;
 
 const SETTINGS = {};
 
-export { MOVEMENT, MAP_OBJECT, RESET_SPEED, DELAY_START };
+export { MOVEMENT, MAP_OBJECT, RESET_SPEED, DELAY_START, COMPENSE_MAP_EDGE_SIZE, ASSETS };
