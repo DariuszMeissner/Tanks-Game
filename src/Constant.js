ae85e9a368453deea075981921b166667247735f
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-19 13:36:21 +0200

    added constant for AssetsLoader feature

--- src/Constant.js
+++ src/Constant.js
@@ -1,26 +1,64 @@
-const MOVEMENT = {
-  forward: "forward",
-  reverse: "reverse",
-  left: "left",
-  right: "right",
-  space: "Space",
+const Movement = {
+  FORWARD: 'forward',
+  REVERSE: 'reverse',
+  LEFT: 'left',
+  RIGHT: 'right',
+  SPACE: 'Space',
 };
 
-const MAP_OBJECT = { road: 0, wall: 1, water: 2, grass: 3, eagle: 4, mapEdge: 5, rock: 6, eagleDead: 7 };
-const ASSETS = {
-  road: "",
-  wall: "./assets/brick_square.png",
-  water: "./assets/water_square.png",
-  grass: "./assets/grass_square.png",
-  eagle: "./assets/eagle_live.png",
-  eagleDead: "./assets/eagle_dead.png",
-  mapEdge: "",
-  rock: "./assets/rock_square.png",
+const MapObject = {
+  ROAD: 0,
+  WALL: 1,
+  WATER: 2,
+  GRASS: 3,
+  EAGLE: 4,
+  MAP_EDGE: 5,
+  ROCK: 6,
+  EAGLE_DEAD: 7,
 };
-const RESET_SPEED = 2;
-const DELAY_START = 2000;
-const COMPENSE_MAP_EDGE_SIZE = 1;
 
-const SETTINGS = {};
+const AssetsType = {
+  IMAGE: 'image',
+  SOUND: 'sound',
+};
 
-export { MOVEMENT, MAP_OBJECT, RESET_SPEED, DELAY_START, COMPENSE_MAP_EDGE_SIZE, ASSETS };
+const AssetsTypeLookup = {
+  png: AssetsType.IMAGE,
+  webp: AssetsType.IMAGE,
+  mp3: AssetsType.SOUND,
+  ogg: AssetsType.SOUND,
+};
+
+const AssetsPathsName = {
+  ROAD: '',
+  WALL: '',
+  WATER: '',
+  GRASS: '',
+  EAGLE: '',
+  EAGLE_DEAD: '',
+  MAP_EDGE: '',
+  ROCK: '',
+  TANK_PLAYER: 'tank-player',
+  TANK_ENEMY: 'tank-enemy',
+};
+
+const AssetsPaths = {
+  ROAD: '',
+  WALL: './assets/brick_square.png',
+  WATER: './assets/water_square.png',
+  GRASS: './assets/grass_square.png',
+  EAGLE: './assets/eagle_live.png',
+  EAGLE_DEAD: './assets/eagle_dead.png',
+  MAP_EDGE: '',
+  ROCK: './assets/rock_square.png',
+  TANK_PLAYER: './assets/tank_player.png',
+  TANK_ENEMY: './assets/tank_enemy.png',
+};
+
+const GameSettings = {
+  RESET_SPEED: 2,
+  DELAY_START: 2000,
+  COMPENSE_MAP_EDGE_SIZE: 1,
+};
+
+export { Movement, MapObject, AssetsType, AssetsPaths, AssetsTypeLookup, GameSettings, AssetsPathsName };
