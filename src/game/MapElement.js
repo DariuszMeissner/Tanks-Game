02eecc43c5ed3e2a1139061775efddd23086538f
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-21 11:15:02 +0200

    fix bug handleCollision method

--- src/game/MapElement.js
+++ src/game/MapElement.js
@@ -1,46 +1,46 @@
-import { DELAY_START, RESET_SPEED } from "./Constant.js";
+import { GameSettings, Movement } from './config/Constant.js';
 
 export default class MapElement {
-  constructor(x, y, tileSize, pathToDesign) {
+  constructor(x, y, tileSize) {
     this.x = x;
     this.y = y;
     this.height = tileSize;
     this.width = tileSize;
     this.compenseSpeed = 2;
-    this.image = new Image();
-    this.image.src = pathToDesign;
   }
 
-  draw(ctx) {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  draw(ctx, image) {
+    if (image instanceof Image) {
+      ctx.drawImage(image, this.x, this.y, this.width, this.height);
+    }
   }
 
   detectCollisionFrontOfTank(object, tank, tankDirection) {
     if (!object || !tank) return false;
 
     switch (tankDirection) {
-      case "forward":
+      case Movement.FORWARD:
         return (
           object.y < tank.y + tank.height &&
           object.y + object.height > tank.y - this.compenseSpeed &&
           object.x < tank.x + tank.width &&
           object.x + object.width > tank.x
         );
-      case "reverse":
+      case Movement.REVERSE:
         return (
           object.y < tank.y + tank.height + this.compenseSpeed &&
           object.y > tank.y &&
           object.x < tank.x + tank.width &&
           object.x + object.width > tank.x
         );
-      case "left":
+      case Movement.LEFT:
         return (
           object.x + object.width > tank.x - this.compenseSpeed &&
           object.x < tank.x + tank.width &&
           object.y < tank.y + tank.height &&
           object.y + object.height > tank.y
         );
-      case "right":
+      case Movement.RIGHT:
         return (
           object.x + object.width > tank.x &&
           object.x < tank.x + tank.width + this.compenseSpeed &&
@@ -59,26 +59,23 @@ export default class MapElement {
       clearTimeout(enemy.timeoutId);
     }
 
-    enemy.isDelayed = true;
-
     enemy.timeoutId = setTimeout(() => {
-      enemy.speed = RESET_SPEED;
-      enemy.isDelayed = false;
+      enemy.speed = GameSettings.RESET_SPEED;
       clearTimeout(enemy.timeoutId);
       enemy.timeoutId = null;
-    }, DELAY_START);
+    }, GameSettings.DELAY_START);
   }
 
   handleCollision(objectOfCollision, tank) {
     if (this.detectCollisionFrontOfTank(objectOfCollision, tank, tank.direction)) {
-      if (!tank.isDelayed) {
-        tank.speed = 0;
-        tank.changeDirection();
+      console.log('collision:', 'id:' + tank.id, tank.x, tank.y, tank.direction, tank.timeoutId);
 
-        if (!this.detectCollisionFrontOfTank(objectOfCollision, tank, tank.direction)) {
-          this.resetSpeedAfterTimeout(tank);
-          return;
-        }
+      tank.speed = 0;
+      tank.changeDirection();
+
+      if (!this.detectCollisionFrontOfTank(objectOfCollision, tank, tank.direction)) {
+        this.resetSpeedAfterTimeout(tank);
+        return;
       }
     }
   }
