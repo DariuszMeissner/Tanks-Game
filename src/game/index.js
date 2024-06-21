31e555e04364b112f240421c90dd1d5d8738bb98
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-21 11:09:46 +0200

    update canvas width variable

--- src/game/index.js
+++ src/game/index.js
@@ -1,11 +1,12 @@
+import { CanvasSize } from './config/Constant.js';
 import { init, main } from './main.js';
 
 function getContext() {
   const canvas = document.getElementById('myCanvas');
   const context = canvas.getContext('2d');
 
-  context.canvas.width = window.innerWidth;
-  context.canvas.height = window.innerHeight;
+  context.canvas.width = CanvasSize.WIDTH;
+  context.canvas.height = CanvasSize.HEIGHT;
   context.imageSmoothingEnabled = false;
 
   return context;
