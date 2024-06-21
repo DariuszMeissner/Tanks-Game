fb5a7e8bc782332f294a5ad3ba742eaa1fb99fc3
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-21 11:09:13 +0200

    refactor and added loading assets before start game

--- src/game/main.js
+++ src/game/main.js
@@ -1,84 +1,53 @@
-// import Tank from './Tank.js';
-// import Bot from './Bot.js';
-// import BulletController from './BulletController.js';
-// import BotController from './BotController.js';
-// import MapController from './MapController.js';
+import Tank from './Tank.js';
+import Bot from './Bot.js';
+import BulletController from './BulletController.js';
+import BotController from './BotController.js';
+import MapController from './MapController.js';
+import AssetsService from './service/AssetsService.js';
+import { AssetsPaths, AssetsPathsName, GameSettings, Map } from './config/Constant.js';
 
-import AssetsLoader from './AssetsLoader.js';
-import { AssetsPaths, AssetsPathsName } from './Constant.js';
+const assetsService = new AssetsService();
 
-// const canvas = document.getElementById('myCanvas');
-// const ctx = canvas.getContext('2d');
-// const tileSize = 40;
+const mapController = new MapController(Map, GameSettings.TILE_SIZE);
+const enemies = [
+  new Bot(1, 160, 180, GameSettings.TILE_SIZE, GameSettings.TILE_SIZE),
+  new Bot(2, 130, 60, GameSettings.TILE_SIZE, GameSettings.TILE_SIZE),
+  new Bot(3, 260, 120, GameSettings.TILE_SIZE, GameSettings.TILE_SIZE),
+];
+const botController = new BotController(enemies);
+const playerBulletController = new BulletController(mapController, botController);
+const player = new Tank(160, 120, GameSettings.TILE_SIZE, GameSettings.TILE_SIZE, playerBulletController);
 
-// // 0 - road
-// // 1 - wall
-// // 2 - water
-// // 3 - grass
-// // 4 - eagle
-// // 5 - edge of map
-// // 6 - rock
-// // 7 - eagle dead
-
-// const map = [
-//   [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-//   [5, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 5],
-//   [5, 0, 1, 0, 0, 0, 1, 0, 0, 0, 2, 2, 2, 0, 5],
-//   [5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5],
-//   [5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 5],
-//   [5, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 5],
-//   [5, 0, 1, 1, 6, 1, 6, 1, 1, 1, 0, 0, 0, 0, 5],
-//   [5, 0, 1, 0, 6, 1, 6, 1, 0, 1, 0, 1, 1, 0, 5],
-//   [5, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 5],
-//   [5, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 5],
-//   [5, 0, 1, 1, 0, 0, 1, 4, 1, 0, 0, 0, 0, 0, 5],
-//   [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-// ];
-
-// const mapController = new MapController(map, tileSize);
-// const enemies = [
-//   new Bot(130, 50, tileSize, tileSize, mapController),
-//   new Bot(130, 180, tileSize, tileSize, mapController),
-//   new Bot(320, 120, tileSize, tileSize, mapController),
-//   new Bot(320, 180, tileSize, tileSize, mapController),
-// ];
-// const botController = new BotController(enemies);
-// const bulletController = new BulletController(ctx, mapController, botController);
-// const tank = new Tank(160, 120, tileSize, tileSize, bulletController, mapController);
-
-// function gameLoop() {
-//   mapController.draw(canvas, ctx, tank, bulletController.bullets[0], botController.enemies);
-//   tank.draw(ctx);
-//   bulletController.draw(ctx, tank);
-//   botController.draw(ctx);
-
-//   requestAnimationFrame(gameLoop);
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   gameLoop();
-// });
-
-const assetsLoader = new AssetsLoader();
+// const bot1BulletController = new BulletController(mapController, [player]);
+// const bot2BulletController = new BulletController(mapController, [player]);
+// const bot3BulletController = new BulletController(mapController, [player]);
 
 export async function init() {
-  await assetsLoader.load([
+  await assetsService.load([
+    [AssetsPathsName.ROAD, AssetsPaths.ROAD],
+    [AssetsPathsName.WALL, AssetsPaths.WALL],
+    [AssetsPathsName.WATER, AssetsPaths.WATER],
+    [AssetsPathsName.GRASS, AssetsPaths.GRASS],
+    [AssetsPathsName.EAGLE, AssetsPaths.EAGLE],
+    [AssetsPathsName.EAGLE_DEAD, AssetsPaths.EAGLE_DEAD],
+    [AssetsPathsName.MAP_EDGE, AssetsPaths.MAP_EDGE],
+    [AssetsPathsName.ROCK, AssetsPaths.ROCK],
     [AssetsPathsName.TANK_PLAYER, AssetsPaths.TANK_PLAYER],
     [AssetsPathsName.TANK_ENEMY, AssetsPaths.TANK_ENEMY],
   ]);
 }
 
 export function main(context, time) {
-  const imageRoad = assetsLoader.assets.get(AssetsPathsName.ROAD);
-  const imageWall = assetsLoader.assets.get(AssetsPathsName.WALL);
-  const imageWater = assetsLoader.assets.get(AssetsPathsName.WATER);
-  const imageGrass = assetsLoader.assets.get(AssetsPathsName.GRASS);
-  const imageEagle = assetsLoader.assets.get(AssetsPathsName.EAGLE);
-  const imageEagleDead = assetsLoader.assets.get(AssetsPathsName.EAGLE_DEAD);
-  const imageMapEdge = assetsLoader.assets.get(AssetsPathsName.MAP_EDGE);
-  const imageRock = assetsLoader.assets.get(AssetsPathsName.ROCK);
-  const imageTankPlayer = assetsLoader.assets.get(AssetsPathsName.TANK_PLAYER);
-  const imageTankEnemy = assetsLoader.assets.get(AssetsPathsName.TANK_ENEMY);
+  const imageRoad = assetsService.assets.get(AssetsPathsName.ROAD);
+  const imageWall = assetsService.assets.get(AssetsPathsName.WALL);
+  const imageWater = assetsService.assets.get(AssetsPathsName.WATER);
+  const imageGrass = assetsService.assets.get(AssetsPathsName.GRASS);
+  const imageEagle = assetsService.assets.get(AssetsPathsName.EAGLE);
+  const imageEagleDead = assetsService.assets.get(AssetsPathsName.EAGLE_DEAD);
+  const imageMapEdge = assetsService.assets.get(AssetsPathsName.MAP_EDGE);
+  const imageRock = assetsService.assets.get(AssetsPathsName.ROCK);
+  const imageTankPlayer = assetsService.assets.get(AssetsPathsName.TANK_PLAYER);
+  const imageTankEnemy = assetsService.assets.get(AssetsPathsName.TANK_ENEMY);
 
   if (
     !imageRoad &&
@@ -94,8 +63,10 @@ export function main(context, time) {
   )
     return;
 
-  context.clearRect(0, 0, context.canvas, context.height);
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
-  context.drawImage(imageTankPlayer, 120, 200, imageTankPlayer.width, imageTankPlayer.height);
-  context.drawImage(imageTankEnemy, 0, 0, imageTankEnemy.width, imageTankEnemy.height);
+  player.draw(context, imageTankPlayer);
+  botController.draw(context, imageTankEnemy);
+  mapController.draw(context, player, playerBulletController.bullets[0], botController.enemies, assetsService.assets);
+  playerBulletController.draw(context, player);
 }
