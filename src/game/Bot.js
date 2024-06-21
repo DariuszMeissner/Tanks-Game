6996c6179335c19f2c409f08a1fe5a7c2f7d9597
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-21 11:04:29 +0200

    refactor and update

--- src/game/Bot.js
+++ src/game/Bot.js
@@ -1,7 +1,8 @@
-import { MOVEMENT } from "./Constant.js";
+import { Movement } from './config/Constant.js';
 
-export default class Enemy {
-  constructor(x, y, width, height, mapController) {
+export default class Bot {
+  constructor(id, x, y, width, height, bulletController) {
+    this.id = id;
     this.x = x;
     this.y = y;
     this.speed = 2;
@@ -10,45 +11,54 @@ export default class Enemy {
     this.previousX = null;
     this.previousY = null;
     this.direction = null;
-    this.directions = ["forward", "reverse", "left", "right"];
+    this.directions = [Movement.FORWARD, Movement.REVERSE, Movement.LEFT, Movement.RIGHT];
     this.bullets = [];
     this.bulletSpeed = 2;
     this.bulletDamage = 1;
-    this.mapController = mapController;
     this.timeoutId = null;
     this.isDelayed = false;
-    this.image = new Image();
-    this.image.src = "assets/tank_enemy_basic.png";
+    this.bulletController = bulletController;
   }
 
-  draw(ctx) {
-    this.move();
+  draw(ctx, image) {
+    if (image instanceof Image) {
+      this.move();
 
-    ctx.save();
-    ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
+      ctx.save();
+      ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
 
-    // Rotate the canvas
-    let angle;
-    switch (this.direction) {
-      case MOVEMENT.forward:
-        angle = 0;
-        break;
-      case MOVEMENT.reverse:
-        angle = Math.PI;
-        break;
-      case MOVEMENT.left:
-        angle = -Math.PI / 2;
-        break;
-      case MOVEMENT.right:
-        angle = Math.PI / 2;
-        break;
+      let angle;
+      switch (this.direction) {
+        case Movement.FORWARD:
+          angle = 0;
+          break;
+        case Movement.REVERSE:
+          angle = Math.PI;
+          break;
+        case Movement.LEFT:
+          angle = -Math.PI / 2;
+          break;
+        case Movement.RIGHT:
+          angle = Math.PI / 2;
+          break;
+      }
+
+      ctx.rotate(angle);
+
+      ctx.drawImage(image, -this.width / 2, -this.height / 2, this.width, this.height);
+
+      ctx.restore();
+
+      // this.shoot();
     }
+  }
 
-    ctx.rotate(angle);
-
-    ctx.drawImage(this.image, -this.width / 2, -this.height / 2, this.width, this.height);
-
-    ctx.restore();
+  shoot() {
+    setInterval(() => {
+      const bulletX = this.x + (this.width / 5) * 2;
+      const bulletY = this.y + this.height / 2;
+      this.bulletController.shoot(bulletX, bulletY, this.bulletSpeed, this.bulletDamage, this.bulletDelay, this.direction);
+    }, 5000);
   }
 
   move() {
@@ -56,23 +66,18 @@ export default class Enemy {
       this.changeDirection();
     }
 
-    const positionTopEdge = this.y <= 0;
-    const positionBottomEdge = this.y >= this.mapController.canvasHeight - this.height;
-    const positionLeftEdge = this.x <= 0;
-    const positionRightEdge = this.x >= this.mapController.canvasWidth - this.width;
-
     switch (this.direction) {
-      case MOVEMENT.forward:
-        positionTopEdge ? (this.y = 0) : (this.y -= this.speed);
+      case Movement.FORWARD:
+        this.y -= this.speed;
         break;
-      case MOVEMENT.reverse:
-        positionBottomEdge ? (this.y = this.mapController.canvasHeight - this.height) : (this.y += this.speed);
+      case Movement.REVERSE:
+        this.y += this.speed;
         break;
-      case MOVEMENT.left:
-        positionLeftEdge ? (this.x = 0) : (this.x -= this.speed);
+      case Movement.LEFT:
+        this.x -= this.speed;
         break;
-      case MOVEMENT.right:
-        positionRightEdge ? (this.x = this.mapController.canvasWidth - this.width) : (this.x += this.speed);
+      case Movement.RIGHT:
+        this.x += this.speed;
         break;
     }
   }
