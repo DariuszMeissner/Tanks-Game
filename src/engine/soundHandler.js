631513d7c89ed9c87858311c98fb61289dcf132b
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-06-28 11:12:57 +0200

    added sound handler methods

---
+++ src/engine/soundHandler.js
@@ -0,0 +1,21 @@
+/**
+ * Play a HTMLAudioElement. This will also reset the playback if it is already currently playing.
+ * @param {HTMLAudioElement} sound
+ * @param {{ volume: number, loop: boolean }} options
+ */
+export function playSound(sound, loop = false) {
+  sound.loop = loop;
+  sound.autoplay = true;
+
+  if (sound.currentTime > 0) sound.currentTime = 0;
+  if (sound.paused) sound.play();
+}
+
+/**
+ * Stop a currently playing HTMLAudioElement, reseting its position to the beginning.
+ * @param {HTMLAudioElement} sound
+ */
+export function pauseSound(sound) {
+  sound.pause();
+  sound.currentTime = 0;
+}
