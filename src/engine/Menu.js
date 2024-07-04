245c1d59acebe5df9ff69db1e66d2d441ea08389
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-04 12:38:03 +0200

    created abstract class Menu and handle moving

---
+++ src/engine/Menu.js
@@ -0,0 +1,70 @@
+const DEFAULT_MENU_SELECTED = 0;
+
+export class Menu {
+  selectedOptions = DEFAULT_MENU_SELECTED;
+  menuOptions;
+
+  constructor() {
+    this.keyStates = {
+      ArrowUp: false,
+      ArrowDown: false,
+      Enter: false,
+    };
+
+    document.addEventListener('keydown', this.#keydown.bind(this));
+    document.addEventListener('keyup', this.#keyup.bind(this));
+  }
+
+  drawMenu(context) {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+    this.highlightSelectedOptions(context);
+    this.move();
+  }
+
+  highlightSelectedOptions(context) {
+    context.font = '40px Arial';
+
+    this.menuOptions.forEach((option, index) => {
+      if (index === this.selectedOptions) {
+        context.fillStyle = 'green';
+      } else {
+        context.fillStyle = 'red';
+      }
+
+      context.fillText(option, 50, 50 + index * 50);
+    });
+  }
+
+  move() {
+    if (this.keyStates.ArrowUp) {
+      this.selectedOptions = this.selectedOptions - 1 < 0 ? this.menuOptions.length - 1 : this.selectedOptions - 1;
+      this.keyStates.ArrowUp = false;
+      return;
+    }
+
+    if (this.keyStates.ArrowDown) {
+      this.selectedOptions = this.selectedOptions + 1 > this.menuOptions.length - 1 ? 0 : this.selectedOptions + 1;
+      this.keyStates.ArrowDown = false;
+      return;
+    }
+  }
+
+  #keydown = (e) => {
+    if (this.keyStates.hasOwnProperty(e.code)) {
+      this.#updateKeyState(e.code);
+    }
+  };
+
+  #keyup = (e) => {
+    if (this.keyStates.hasOwnProperty(e.code)) {
+      this.keyStates[e.code] = false;
+    }
+  };
+
+  #updateKeyState(eventKey) {
+    if (this.keyStates.hasOwnProperty(eventKey)) {
+      this.keyStates[eventKey] = true;
+    }
+  }
+}
