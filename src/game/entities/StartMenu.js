36d2dd9060a41328c1f7588469079a407d01ff32
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-04 14:52:14 +0200

    call drawing main text battle city

--- src/game/entities/StartMenu.js
+++ src/game/entities/StartMenu.js
@@ -1,24 +1,38 @@
 import { Menu } from '../../engine/Menu.js';
-import { MenuOptions, MenuType } from '../constants/game.js';
+import { ImagesPaths, MenuOptions, MenuType } from '../constants/game.js';
 
 export class StartMenu extends Menu {
   constructor() {
     super();
     this.menuOptions = MenuOptions;
+    this.logoImg = new Image();
+    this.logoImg.src = ImagesPaths.MAIN_TEXT;
   }
 
   draw(context, setStage) {
+    this.#clearCanvas(context);
+
+    this.drawLogo(context);
+
     this.drawMenu(context);
 
-    this.handlePressEnter(setStage);
+    this.#handlePressEnter(setStage);
   }
 
-  handlePressEnter(setStage) {
+  drawLogo(context) {
+    context.drawImage(this.logoImg, 0, 0);
+  }
+
+  #handlePressEnter(setStage) {
     if (this.keyStates.Enter) {
       this.selectedOptions === 0 && setStage(MenuType.PLAYER_1);
       this.selectedOptions === 1 && setStage(MenuType.PLAYERS_2);
 
       this.keyStates.Enter = false;
     }
+  }
+
+  #clearCanvas(context) {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
   }
 }
