bd2d3bfa93841529cbd4a4643ed07615002da0a7
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-04 12:49:02 +0200

    added flags to handle active scene and start menu

--- src/engine/Game.js
+++ src/engine/Game.js
@@ -1,19 +1,42 @@
+import { MenuType } from '../game/constants/game.js';
 import { getContext } from './context.js';
 
 export default class Game {
   scene;
+  startMenu;
 
   constructor(width, height) {
     this.context = getContext(width, height);
+    this.activateScene = false;
+    this.activateStartMenu = true;
+    this.setStage = this.setStage.bind(this);
   }
 
   frame = (time) => {
     window.requestAnimationFrame(this.frame);
 
-    if (this.scene.draw) this.scene.draw(this.context);
+    if (this.startMenu && this.activateStartMenu) {
+      this.startMenu.draw(this.context, this.setStage);
+    }
+
+    if (this.activateScene) {
+      this.scene.draw(this.context);
+    }
   };
 
   start() {
     window.requestAnimationFrame(this.frame);
+  }
+
+  setStage(stage) {
+    switch (stage) {
+      case MenuType.PLAYER_1:
+        this.activateScene = true;
+        this.activateStartMenu = false;
+        break;
+      case MenuType.PLAYERS_2:
+        this.activateScene = true;
+        break;
+    }
   }
 }
