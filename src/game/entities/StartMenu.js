8ec56cf50e263e00bfef9435ecbf99b748660a2e
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-05 11:38:21 +0200

    position logo and menu at the canvas, added cursor to menu position

--- src/game/entities/StartMenu.js
+++ src/game/entities/StartMenu.js
@@ -1,5 +1,6 @@
 import { Menu } from '../../engine/Menu.js';
-import { ImagesPathsName, MenuOptions, MenuType } from '../constants/game.js';
+import { alignCenterImage, scaleImage } from '../../engine/util/ui.js';
+import { Colors, FONT, ImagesPathsName, MenuOptions, MenuType, SCREEN_HEIGHT, SCREEN_WIDTH } from '../constants/game.js';
 
 export class StartMenu extends Menu {
   constructor(assets) {
@@ -11,15 +12,51 @@ export class StartMenu extends Menu {
   draw(context, setStage) {
     this.#clearCanvas(context);
 
-    this.drawLogo(context);
+    this.logo(context);
 
-    this.drawMenu(context);
+    this.menu(context);
 
     this.#handlePressEnter(setStage);
   }
 
-  drawLogo(context) {
-    context.drawImage(this.assets.get(ImagesPathsName.MAIN_TEXT), 0, 0);
+  menu(context) {
+    this.setStyles(context);
+    this.move();
+  }
+
+  logo(context) {
+    const logo = this.assets.get(ImagesPathsName.MAIN_TEXT);
+    const logoScale = scaleImage(logo, 1);
+    const center = alignCenterImage(logoScale.width);
+
+    context.drawImage(logo, center, SCREEN_HEIGHT / 6, logoScale.width, logoScale.height);
+  }
+
+  setStyles(context) {
+    context.font = `24px ${FONT}`;
+
+    this.menuOptions.forEach((option, index) => {
+      this.cursorOption(index, context);
+
+      context.fillStyle = Colors.WHITE;
+      context.textAlign = 'center';
+      context.fillText(option, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + index * 40);
+    });
+  }
+
+  cursorOption(index, context) {
+    const cursor = this.assets.get(ImagesPathsName.TANK_PLAYER);
+    const cursorScale = scaleImage(cursor, 0.065);
+
+    if (index === this.selectedOptions) {
+      context.drawImage(
+        cursor,
+        SCREEN_WIDTH / 2 - 160,
+        SCREEN_HEIGHT / 2 - cursorScale.height + index * 40,
+        cursorScale.width,
+        cursorScale.height
+      );
+    }
   }
 
   #handlePressEnter(setStage) {
