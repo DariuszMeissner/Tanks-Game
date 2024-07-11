3d270178b39787cd01c30e2d190896590bfc6684
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-11 09:56:03 +0200

    added flag maxTankOnMap to controll number of tanks on the map

--- src/game/entities/BotController.js
+++ src/game/entities/BotController.js
@@ -1,17 +1,13 @@
 import BulletController from '../entities/BulletController.js';
 
 export default class BotController {
-  constructor(enemies, mapController, playersController, maxBotOnMap) {
+  constructor(enemies, mapController, playersController, maxTankOnMap) {
     this.enemies = enemies.map((enemy) => {
       enemy.bulletController = new BulletController(mapController, playersController);
       return enemy;
     });
     this.mapController = mapController;
-    this.maxBotOnMap = maxBotOnMap;
-  }
-
-  endGame() {
-    this.mapController.winGame = true;
+    this.maxTankOnMap = maxTankOnMap;
   }
 
   draw(context, image) {
@@ -22,10 +18,14 @@ export default class BotController {
     }
 
     this.enemies.forEach((bot, index) => {
-      if (index < this.maxBotOnMap) {
+      if (index < this.maxTankOnMap) {
         bot.draw(context, image);
         bot.bulletController.draw(context, bot);
       }
     });
+  }
+
+  endGame() {
+    this.mapController.winGame = true;
   }
 }
--- src/game/entities/PlayersController.js
+++ src/game/entities/PlayersController.js
@@ -1,17 +1,30 @@
 import BulletController from './BulletController.js';
 
 export default class PlayersController {
-  constructor(players, mapController) {
+  constructor(players, mapController, maxTankOnMap) {
     this.enemies = players.map((player) => {
       player.bulletController = new BulletController(mapController, null);
       return player;
     });
+    this.maxTankOnMap = maxTankOnMap;
   }
 
   draw(ctx, image) {
-    this.enemies.forEach((player) => {
-      player.draw(ctx, image);
-      player.bulletController.draw(ctx, player);
+    const noLifes = this.enemies.length === 0;
+    if (noLifes) {
+      this.endGame();
+      return;
+    }
+
+    this.enemies.forEach((player, index) => {
+      if (index < this.maxTankOnMap) {
+        player.draw(ctx, image);
+        player.bulletController.draw(ctx, player);
+      }
     });
+  }
+
+  endGame() {
+    this.mapController.gameOver = true;
   }
 }
