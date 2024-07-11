2f9f785ebdf432ab04f5a710e9180f2dfb040aac
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-11 09:40:54 +0200

    init HUD display

---
+++ src/game/entities/Hud.js
@@ -0,0 +1,52 @@
+import { Panel } from '../../engine/Panel';
+import { scaleImage } from '../../engine/util/ui';
+import { Colors, FONT, ImagesPathsName, MAP_WIDTH, TILE_SIZE_WIDTH } from '../constants/game';
+
+export class Hud extends Panel {
+  constructor() {
+    super();
+  }
+
+  draw(context) {
+    this.#panel(context);
+  }
+
+  #panel(context) {
+    context.font = `16px ${FONT}`;
+    context.fillStyle = Colors.BLACK;
+
+    this.botController.enemies.forEach((_, index) => {
+      context.fillText('|', TILE_SIZE_WIDTH * MAP_WIDTH - TILE_SIZE_WIDTH / 1.5, 100 + 20 * index);
+    });
+
+    this.#playerLifeInfo(context);
+    this.#stageLevelInfo(context);
+  }
+
+  #stageLevelInfo(context) {
+    const stageFlag = this.assets.get(ImagesPathsName.STAGE_FLAG);
+    const stageFlagScale = scaleImage(stageFlag, 0.6);
+    const setCenterOnBoardImage = TILE_SIZE_WIDTH * (MAP_WIDTH - 1) - stageFlagScale.width / 2;
+    const setCenterOnBoardText = TILE_SIZE_WIDTH * (MAP_WIDTH - 1) + stageFlagScale.width / 2;
+
+    context.font = `20px ${FONT}`;
+
+    context.fillText('I P', (TILE_SIZE_WIDTH * (MAP_WIDTH - 1) + TILE_SIZE_WIDTH * (MAP_WIDTH - 2)) / 2, 180);
+    context.fillText(
+      this.playersController.enemies.length,
+      (TILE_SIZE_WIDTH * (MAP_WIDTH - 1) + TILE_SIZE_WIDTH * (MAP_WIDTH - 2)) / 2,
+      220
+    );
+
+    context.drawImage(stageFlag, setCenterOnBoardImage, 220, stageFlagScale.width, stageFlagScale.height);
+
+    context.fillStyle = Colors.BLACK;
+    context.textAlign = 'right';
+
+    context.fillText(this.stageLevel, setCenterOnBoardText, 280);
+  }
+
+  #playerLifeInfo(context) {
+    context.font = `20px ${FONT}`;
+  }
+}
