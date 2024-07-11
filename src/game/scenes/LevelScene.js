2b7afce1e6f63b0f65670318b234f756c11d8402
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-11 10:08:58 +0200

    move players lifes initialization from TanksGame class and hud display drawing initialization

--- src/game/scenes/LevelScene.js
+++ src/game/scenes/LevelScene.js
@@ -3,8 +3,18 @@ import BotController from '../entities/BotController.js';
 import MapController from '../entities/MapController.js';
 import PlayersController from '../entities/PlayersController.js';
 import Scene from '../../engine/Scene.js';
-import { TILE_SIZE_WIDTH, BOT_WIDTH, BOT_HEIGHT, ImagesPathsName, SoundsPathsName } from '../constants/game.js';
+import {
+  TILE_SIZE_WIDTH,
+  BOT_WIDTH,
+  BOT_HEIGHT,
+  ImagesPathsName,
+  SoundsPathsName,
+  PLAYER_WIDTH,
+  PLAYER_HEIGHT,
+} from '../constants/game.js';
 import { clearCanvas } from '../../engine/util/ui.js';
+import Player from '../entities/Player.js';
+import { Hud } from '../entities/Hud.js';
 
 const enemies = [
   new Bot(200, 210, BOT_WIDTH, BOT_HEIGHT),
@@ -13,18 +23,24 @@ const enemies = [
   new Bot(200, 210, BOT_WIDTH, BOT_HEIGHT),
 ];
 
+const life1 = new Player(160, 120, PLAYER_WIDTH, PLAYER_HEIGHT, null);
+const life2 = new Player(160, 120, PLAYER_WIDTH, PLAYER_HEIGHT, null);
+const life3 = new Player(160, 120, PLAYER_WIDTH, PLAYER_HEIGHT, null);
+
+const players = [life1, life2, life3];
+
 export class LevelScene extends Scene {
-  constructor(assets, players, stageLevel, maxTankOnMap) {
+  constructor(assets, stageLevel, maxTankOnMap) {
     super();
     this.stageLevel = stageLevel[0].stage;
-    this.player1 = players[0];
     this.assets = assets;
     this.stage = new MapController(stageLevel.slice(1), TILE_SIZE_WIDTH);
     this.maxTankOnMap = maxTankOnMap;
-    this.playersController = new PlayersController(players, this.stage);
+    this.playersController = new PlayersController(players, this.stage, 1);
     this.botController = new BotController(enemies, this.stage, this.playersController, maxTankOnMap);
+    this.hud = new Hud(this.playersController, this.botController, assets, this.stageLevel);
 
-    this.fixPlayersBulletsCircularDependency(this.player1);
+    this.fixPlayersBulletsCircularDependency(players);
   }
 
   draw(context) {
@@ -46,5 +62,7 @@ export class LevelScene extends Scene {
     this.showWictoryInfo(context, this.stage);
 
     this.showGameOverInfo(context, this.stage);
+
+    this.hud.draw(context);
   }
 }
