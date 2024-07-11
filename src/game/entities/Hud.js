9144b8f61d4b09dbd857be7d18a6463792f20921
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-11 14:26:12 +0200

    HUD: enemy health displayed in two columns

--- src/game/entities/Hud.js
+++ src/game/entities/Hud.js
@@ -1,6 +1,6 @@
 import { Panel } from '../../engine/Panel.js';
 import { scaleImage } from '../../engine/util/ui.js';
-import { Colors, FONT, ImagesPathsName, MAP_WIDTH, TILE_SIZE_WIDTH } from '../constants/game.js';
+import { Colors, FONT, HudP, ImagesPathsName, MAP_WIDTH, TILE_SIZE_WIDTH } from '../constants/game.js';
 
 export class Hud extends Panel {
   constructor(playersController, botController, assets, stageLevel) {
@@ -16,18 +16,41 @@ export class Hud extends Panel {
   }
 
   #panel(context) {
-    context.font = `16px ${FONT}`;
-    context.fillStyle = Colors.BLACK;
-
-    this.botController.enemies.forEach((_, index) => {
-      context.fillText('|', TILE_SIZE_WIDTH * MAP_WIDTH - TILE_SIZE_WIDTH / 1.5, 100 + 20 * index);
-    });
-
-    this.#playerLifeInfo(context);
-    this.#stageLevelInfo(context);
+    context.save();
+    this.#botLife(context);
+    // this.#playerLife(context);
+    // this.#stageLevel(context);
+    context.restore();
   }
 
-  #stageLevelInfo(context) {
+  #botLife(context) {
+    context.save();
+    const enemy = this.assets.get(ImagesPathsName.ENEMY_LIFE);
+    const enemyScale = scaleImage(enemy, 0.7);
+    const alignFirstColumn = HudP.MIDDLE - enemyScale.width;
+    const alignSecondColumn = HudP.MIDDLE;
+
+    this.botController.enemies.forEach((_, index) => {
+      const nextRow = 40 + enemyScale.height * (index / 2);
+      const currentRow = 40 + enemyScale.height * (Math.round(index / 2) - 1);
+
+      if (index % 2 == 0) {
+        context.drawImage(enemy, alignFirstColumn, nextRow, enemyScale.width, enemyScale.height);
+      } else {
+        context.drawImage(enemy, alignSecondColumn, currentRow, enemyScale.width, enemyScale.height);
+      }
+    });
+    context.restore();
+  }
+
+  #playerLife(context) {
+    context.save();
+    context.font = `20px ${FONT}`;
+    context.resoter();
+  }
+
+  #stageLevel(context) {
+    context.save();
     const stageFlag = this.assets.get(ImagesPathsName.STAGE_FLAG);
     const stageFlagScale = scaleImage(stageFlag, 0.6);
     const setCenterOnBoardImage = TILE_SIZE_WIDTH * (MAP_WIDTH - 1) - stageFlagScale.width / 2;
@@ -48,9 +71,6 @@ export class Hud extends Panel {
     context.textAlign = 'right';
 
     context.fillText(this.stageLevel, setCenterOnBoardText, 280);
-  }
-
-  #playerLifeInfo(context) {
-    context.font = `20px ${FONT}`;
+    context.restore();
   }
 }
