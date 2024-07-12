769e0f91f68a9f64ed799c9c16e4bba8c4ce63fe
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-12 11:48:12 +0200

    refactor options methods menu

--- src/game/entities/StartMenu.js
+++ src/game/entities/StartMenu.js
@@ -1,6 +1,6 @@
 import { Menu } from '../../engine/Menu.js';
 import { alignCenterImage, clearCanvas, scaleImage } from '../../engine/util/ui.js';
-import { Colors, FONT, ImagesPathsName, MenuOptions, MenuType, SCREEN_HEIGHT, SCREEN_WIDTH } from '../constants/game.js';
+import { Colors, FONT, ImagesPathsName, MapP, MenuOptions, MenuType, SCREEN_HEIGHT } from '../constants/game.js';
 
 export class StartMenu extends Menu {
   constructor(assets) {
@@ -33,27 +33,32 @@ export class StartMenu extends Menu {
   }
 
   #setStyles(context) {
-    context.font = `20px ${FONT}`;
+    context.save();
+    const lineHeight = 40;
+    const offsetMenu = 40;
 
     this.menuOptions.forEach((option, index) => {
       this.#cursorOption(index, context);
 
+      context.font = `20px ${FONT}`;
       context.fillStyle = Colors.WHITE;
       context.textAlign = 'left';
-      context.fillText(option, SCREEN_WIDTH / 2.5, SCREEN_HEIGHT / 2 + index * 40);
+      context.fillText(option, MapP.MIDDLE.X - offsetMenu, MapP.MIDDLE.Y + index * lineHeight);
     });
+    context.restore();
   }
 
   #cursorOption(index, context) {
     const cursor = this.assets.get(ImagesPathsName.CURSOR);
     const cursorScale = scaleImage(cursor, 0.065);
-    const offset = 120;
+    const offset = 100;
+    const lineHeight = 40;
 
     if (index === this.selectedOptions) {
       context.drawImage(
         cursor,
-        SCREEN_WIDTH / 2 - offset,
-        SCREEN_HEIGHT / 2 - cursorScale.height / 1.25 + index * 40,
+        MapP.MIDDLE.X - offset,
+        MapP.MIDDLE.Y - cursorScale.height / 1.25 + index * lineHeight,
         cursorScale.width,
         cursorScale.height
       );
