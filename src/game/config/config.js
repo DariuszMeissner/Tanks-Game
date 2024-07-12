815087e9c9150c5d3e6cb48729646bf65a534913
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-12 10:08:30 +0200

    created respawn places for bots and pLayer

---
+++ src/game/config/config.js
@@ -0,0 +1,23 @@
+import { MAP_HEIGHT, MAP_WIDTH, TILE_SIZE_HEIGHT, TILE_SIZE_WIDTH } from '../constants/game.js';
+
+const HUD_EDGE_WIDTH = 3;
+
+export const BotRespawn = {
+  LEFT: {
+    X: 1 * TILE_SIZE_WIDTH,
+    Y: 1 * TILE_SIZE_HEIGHT,
+  },
+  MIDDLE: {
+    X: ((MAP_WIDTH - HUD_EDGE_WIDTH) / 2) * TILE_SIZE_WIDTH,
+    Y: 1 * TILE_SIZE_HEIGHT,
+  },
+  RIGHT: {
+    X: (MAP_WIDTH - HUD_EDGE_WIDTH) * TILE_SIZE_WIDTH,
+    Y: 1 * TILE_SIZE_HEIGHT,
+  },
+};
+
+export const PlayerRespawn = {
+  X: Math.floor((MAP_WIDTH - HUD_EDGE_WIDTH) / 2 - 1) * TILE_SIZE_WIDTH,
+  Y: (MAP_HEIGHT - 2) * TILE_SIZE_HEIGHT,
+};
