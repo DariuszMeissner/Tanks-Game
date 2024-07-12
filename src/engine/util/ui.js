dac49f73a58a6fe557a03f9fba382a62079c498b
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-12 11:46:43 +0200

    removed unused method showNotification

--- src/engine/util/ui.js
+++ src/engine/util/ui.js
@@ -1,10 +1,4 @@
-import { FONT, SCREEN_HEIGHT, SCREEN_WIDTH } from '../../game/constants/game.js';
-
-export function showNotification(text, context, size, color) {
-  context.font = `${size}px ${FONT}`;
-  context.fillStyle = color;
-  context.fillText(text, SCREEN_WIDTH / 2.5, SCREEN_HEIGHT / 2);
-}
+import { SCREEN_WIDTH } from '../../game/constants/game.js';
 
 export function alignCenterImage(imageWidth) {
   return SCREEN_WIDTH / 2 - imageWidth / 2;
