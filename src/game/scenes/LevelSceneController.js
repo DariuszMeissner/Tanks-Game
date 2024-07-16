193b500f8ff0d865e7ed5aecee0bfb5aa49254a2
DariuszMeissner <dariusz.r.meissner@gmail.com>
2024-07-16 12:22:24 +0200

    feature: players life move from level to level

--- src/game/scenes/LevelSceneController.js
+++ src/game/scenes/LevelSceneController.js
@@ -1,18 +1,19 @@
 import { clearCanvas, generateBots, generatePlayers } from '../common/common.js';
-import { LEVEL_INIT } from '../config/config.js';
+import { LEVEL_INIT, PlayerRespawn } from '../config/config.js';
+import { Control } from '../constants/controls.js';
 import { FONT, SCREEN_HEIGHT, SCREEN_WIDTH } from '../constants/game.js';
 import { MAP_LEVELS } from '../constants/levelsMaps.js';
 import { LevelScene } from './LevelScene.js';
 
 const EnemiesInit = generateBots(2);
-
 const PlayersInit = generatePlayers(3);
 
 export class LevelSceneController {
   constructor(assets) {
+    this.players = PlayersInit;
     this.assets = assets;
     this.currentLevel = LEVEL_INIT;
-    this.stageController = new LevelScene(EnemiesInit, PlayersInit, assets, MAP_LEVELS.get(this.currentLevel.toString()), 1);
+    this.stageController = new LevelScene(EnemiesInit, this.players, assets, MAP_LEVELS.get(this.currentLevel.toString()), 1);
     this.endGame = false;
   }
 
@@ -41,8 +42,17 @@ export class LevelSceneController {
     }
 
     const enemies = generateBots(20);
-    const players = generatePlayers(3);
-    this.stageController = new LevelScene(enemies, players, this.assets, currentLevelData, 1);
+    this.#resetPlayerPosition();
+    this.stageController = new LevelScene(enemies, this.players, this.assets, currentLevelData, 1);
+  }
+
+  #resetPlayerPosition() {
+    this.players = this.#getThePlayersLifeCount();
+    this.players.forEach((player) => {
+      player.x = PlayerRespawn.X;
+      player.y = PlayerRespawn.Y;
+      player.direction = Control.UP;
+    });
   }
 
   #endGame(context) {
@@ -53,5 +63,9 @@ export class LevelSceneController {
     context.fillText('THANK YOU FOR THE GAME!', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
     context.fillText('CREATED BY DARIUSZ MEISSNER.', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + lineHeight);
     context.restore();
+  }
+
+  #getThePlayersLifeCount() {
+    return this.stageController.playersController.enemies;
   }
 }
